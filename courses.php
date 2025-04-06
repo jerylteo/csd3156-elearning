4270aa5c3559055441f03acffe67222f8bb0c1c6
Jeryl <52659891+jerylteo@users.noreply.github.com>
2025-04-06 18:34:48 +0800

    test

--- courses.php
+++ courses.php
@@ -111,7 +111,7 @@ if ($course_id > 0) {
                       <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
                         <?php echo htmlspecialchars($courseInfo['COURSENAME']); ?>
                       </h3>
-                      <button data-modal-show="default-modal" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+                      <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
                           <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                           </svg>
@@ -162,7 +162,7 @@ if ($course_id > 0) {
                   <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">' . htmlspecialchars($row['COURSENAME']) . '</h5>
                 </a>
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">' . htmlspecialchars($row['CATEGORY']) . '</p>
-                <a href="modules.php?course=' . htmlspecialchars($row['ID']) . '" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <a href="courses.php?course=' . htmlspecialchars($row['ID']) . '" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                   View Modules
                   <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
