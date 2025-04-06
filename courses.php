2e20486604fc8fdc880c9fd03d39d2cc72bb268b
Jeryl <52659891+jerylteo@users.noreply.github.com>
2025-04-06 18:59:25 +0800

    test

--- courses.php
+++ courses.php
@@ -161,7 +161,7 @@ if ($course_id > 0) {
                   <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">' . htmlspecialchars($row['COURSENAME']) . '</h5>
                 </a>
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">' . htmlspecialchars($row['CATEGORY']) . '</p>
-                <a href="" data-course-id="' . htmlspecialchars($row['ID']) .'" data-course-name="'. htmlspecialchars($row['COURSENAME']).'" class="view-course-btn inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <a href="courses.php?course='. htmlspecialchars($row['ID']) .'" class="view-course-btn inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                   View Modules
                   <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
@@ -198,20 +198,20 @@ if ($course_id > 0) {
     }
   });
 
-  document.querySelectorAll('.view-course-btn').forEach(btn => {
-  btn.addEventListener('click', e => {
-    e.preventDefault();
-    const id = btn.getAttribute('data-course-id');
-    const name = btn.getAttribute('data-course-name');
+  // document.querySelectorAll('.view-course-btn').forEach(btn => {
+  // btn.addEventListener('click', e => {
+  //   e.preventDefault();
+  //   const id = btn.getAttribute('data-course-id');
+  //   const name = btn.getAttribute('data-course-name');
 
 
-    // Update modal content dynamically
-    document.querySelector('#default-modal h3').innerText = name;
+  //   // Update modal content dynamically
+  //   document.querySelector('#default-modal h3').innerText = name;
 
-    // Show modal
-    const modal = new Modal(document.getElementById('default-modal'));
-    modal.show();
-  });
+  //   // Show modal
+  //   const modal = new Modal(document.getElementById('default-modal'));
+  //   modal.show();
+  // });
 });
 </script>
 <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
